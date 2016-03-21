77c02335a41c96df4f7f8c28be68cbe1115ed431
akira1110 <eerf0309@gmail.com>
2016-03-21 13:34:31 +0900

    前処理

--- Preprocessing/create_data.sh
+++ Preprocessing/create_data.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+#下から15行は不要
+#体重532kgは53.2kgのうち間違えかも
 
 more data_source.csv | head -`expr 69627 - 15` | sed -e "s@532\.8@53.2@g" > .data_temp.csv
 
 uniqw .data_temp.csv 10 > data.csv
 rm .data_temp.csv
-
